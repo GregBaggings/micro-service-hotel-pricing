b93d2e34a31ecb50ee453a642bfcbca1d23c9776
GregBaggings <agnegerg@gmail.com>
2017-09-22 16:17:10 +0200

    Database changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO prices (hotelid,roomid,roomname,price) VALUES ('1','1','Double Room','5000');
-INSERT INTO prices (hotelid,roomid,roomname,price) VALUES ('1','2','Single Room','2500');
-INSERT INTO prices (hotelid,roomid,roomname,price) VALUES ('2','3','Elite Room','10000');
+INSERT INTO prices (hotelid,roomid,price) VALUES ('1','1','5000');
+INSERT INTO prices (hotelid,roomid,price) VALUES ('1','2','2500');
+INSERT INTO prices (hotelid,roomid,price) VALUES ('2','3','10000');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,5 @@ CREATE TABLE prices
 (
 hotelid int NOT NULL,
 roomid int NOT NULL IDENTITY,
-roomname varchar(50),
 price int
 );
