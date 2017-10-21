0ff6016ce69a699176713c928a1b628dbe14eae5
GregBaggings <agnegerg@gmail.com>
2017-10-21 16:47:31 +0200

    added new data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO prices (hotelid,roomid,price) VALUES ('1','1','5000');
 INSERT INTO prices (hotelid,roomid,price) VALUES ('1','2','2500');
 INSERT INTO prices (hotelid,roomid,price) VALUES ('2','3','10000');
+INSERT INTO prices (hotelid,roomid,price) VALUES ('5','4','100');
+INSERT INTO prices (hotelid,roomid,price) VALUES ('5','5','15000');
