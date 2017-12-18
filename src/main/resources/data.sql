984d6a6dd77824808eb46e0abc6a0ca6b9515d5a
GregBaggings <agnegerg@gmail.com>
2017-12-18 21:14:00 +0100

    Price change

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT INTO prices (hotelid,roomid,price) VALUES ('1','1','5000');
 INSERT INTO prices (hotelid,roomid,price) VALUES ('1','2','2500');
 INSERT INTO prices (hotelid,roomid,price) VALUES ('2','3','10000');
 INSERT INTO prices (hotelid,roomid,price) VALUES ('5','4','100');
-INSERT INTO prices (hotelid,roomid,price) VALUES ('5','5','15000');
+INSERT INTO prices (hotelid,roomid,price) VALUES ('5','5','100');
